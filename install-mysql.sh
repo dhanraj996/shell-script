eff598b24280d98bbf689d5f12f44a677107c20f
dhanraj996 <luckydhana008@gmail.com>
2025-01-06 19:43:51 +0530

    mysql-installed

---
+++ install-mysql.sh
@@ -0,0 +1,30 @@
+#!bin/bash
+
+#chek the user have root access or not
+
+USERID = $(id -u)
+
+if[ $USERID -ne 0 ]
+
+then
+    echo "ERROR:: you must have the root access to run the script"
+    exit 1
+fi
+
+#check mysql is alredy install or not ?
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    dnf install mysql -y
+    if [ $? -ne 0]
+    then
+        echo "Installed mysql.....failure"
+
+    else
+        echo "Installed mysql.....succefull"
+    fi
+else
+    echo "mysql already... installed"
+fi
